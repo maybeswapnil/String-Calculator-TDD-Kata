097fde2bca6bf824769afc5de99c924cd1a22467
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2022-04-12 13:51:10 +0530

    should allow //[*][%]\\n1*2%3%9 in between the input number string

--- String-Calculator-TDD-Kata.js
+++ String-Calculator-TDD-Kata.js
@@ -27,7 +27,11 @@ describe("String Calculator",  function(){
     });
     //3rd
     it("should allow \\n in between the input number string", function(){
-    expect(calculator.add("1\n2\n3")).equal(6);
+        expect(calculator.add("1\n2\n3")).equal(6);
+    });
+    //4th
+    it("should allow //[*][%]\\n1*2%3%9 in between the input number string", function(){
+        expect(calculator.add("//[*][%]\n1*2%3%9")).equal(15);
     });
 
 });
@@ -39,9 +43,19 @@ function StringCalculator(){
 StringCalculator.prototype.add = function(stringNumbers) {
     //1st: if nothing is passed as an argument
     if(stringNumbers.length===0) return 0;
+    //4th: deal with delimitter
+    const numbers = stringNumbers
+    .replace(/(\r\n|\n|\r)/gm, ',') // Normalize delimiter
+    .split(',')                     // Split values
+    .map(n => parseInt(n, 10))      // Convert to integers
+
+    if(stringNumbers.slice(0, 2) == '//') {
+        let delimitter = RegExp(numbers[2], "g") //delimitter: /(?:)/g
+        stringNumbers = stringNumbers.replace(stringNumbers.slice(0, 3), "") //remove starting delimitter string
+        stringNumbers = stringNumbers.replace(delimitter, ",") //replace delimitter values between the string
+    }
     //3rd: for converting '\n' into ','
     if(stringNumbers.indexOf("\n") !== -1) stringNumbers = stringNumbers.replace(/\n/g, ",")
     //2nd: for unknown number of arguments seperated by ','
-    if(stringNumbers.indexOf(',') !== -1) return parseInt(stringNumbers.split(',').reduce((a, b) => parseInt(a) + parseInt(b)));
-
+    if(stringNumbers.indexOf(',') !== -1) return parseInt(stringNumbers.split(',').filter(i => parseInt(i)<1000).reduce((a, b) => parseInt(a) + parseInt(b))); //changes in 4th commit: added filter
 };
