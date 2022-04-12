27e31edfa6213e77b2fb173405ec02e8564edcc6
Swapnil Sharma <swapnil.sharma1998@outlook.com>
2022-04-12 13:35:06 +0530

    should return the sum of an unknown amount of numbers

--- String-Calculator-TDD-Kata.js
+++ String-Calculator-TDD-Kata.js
@@ -12,6 +12,19 @@ describe("String Calculator",  function(){
     it("should return zero when '' is passed", function(){
         expect(calculator.add('')).equal(0);
     });
+    //2nd
+    it("should return the sum of an unknown amount of numbers", function(){
+        // creates a random array of input numbers
+        randomArray = (length, max) => [...new Array(length)]
+        .map(() => Math.round(Math.random() * max));
+        // creates a random number between 1 & 100
+        rand = Math.floor((Math.random() * 100) + 1);
+        res = randomArray(rand,rand);
+        sum = res.reduce((pv, cv) => pv+cv, 0);
+        // converts the array to a string
+        arg = res.join();
+        expect(calculator.add(arg)).equal(sum);
+      });
 
 });
 
@@ -20,7 +33,11 @@ function StringCalculator(){
 }
 
 StringCalculator.prototype.add = function(stringNumbers) {
-
+    //1st
     if(stringNumbers.length===0) return 0;
+    //2nd
+    if(stringNumbers.indexOf(',') !== -1) {
+        return parseInt(stringNumbers.split(',').reduce((a, b) => parseInt(a) + parseInt(b)))
+    } 
 
 };
